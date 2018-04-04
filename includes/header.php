957e6447b8cc25ce1b5613bcff4787623bc12249
christopher <christopher@sfr.fr>
2018-04-04 14:39:30 +0200

    ecriture menu header

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,10 @@
-<h1>header</h1>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php?page=home">Acceuil</a></li>
+            <li><a href="index.php?page=registration">Inscription</a></li>
+            <li><a href="index.php?page=contact">Contact</a></li>
+            <li><a href="index.php?page=news">News</a></li>
+        </ul>
+    </nav>
+</header>
