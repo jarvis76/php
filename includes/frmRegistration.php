ced66a9717f06d7a571ba2cc72c8579d05e77487
christopher <christopher@sfr.fr>
2018-04-05 09:28:25 +0200

    formulaire inscription

---
+++ includes/frmRegistration.php
@@ -0,0 +1,24 @@
+<form method="post" action="#">
+    <div>
+        <label for="nom">Nom&nbsp;:</label> 
+        <imput type="text" name="nom"/>
+    </div>
+    <div>
+        <label for="prenom">Prenom&nbsp;:</label> 
+        <imput type="text" name="prenom"/>
+    </div>
+    <div>
+        <label for="mail">Mail&nbsp;:</label> 
+        <imput type="text" name="mail"/>
+    </div>
+    <div>
+        <label for="mdp">Mot de passe&nbsp;:</label> 
+        <imput type="password" name="mdp"/>
+    </div>
+    <div>
+        <label for="reset">Effacer&nbsp;:</label> 
+        <imput type="submit" name="Envoyer"/>
+    </div>
+    <imput type="hidden" name="frmRegistration"/>
+
+</form>
