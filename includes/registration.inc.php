6f11aa11ef8cdc0b29135373505df06421c696dc
christopher <christopher@sfr.fr>
2018-04-06 14:03:52 +0200

    mise a jour registration

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -64,11 +64,14 @@ if (isset($_POST['frmRegistration'])){
     else {
 
         $mdp = sha1($mdp);
+
+        $token = uniqid(sha1(date('Y-m-d|H:m:s')), false);
+
         //adresse serveur / nom utilisateur / mdp / nom basse de donne
         $connection = mysqli_connect("localhost", "christopher", "talen", "phpdieppe");
         $requete = "INSERT INTO T_USERS
-                    (USENAME, USEFIRSTNAME, USEMAIL, USEPASSWORD, ID_ROLE)
-                    VALUES ('$nom', '$prenom', '$mail', '$mdp', 3)";
+                    (USENAME, USEFIRSTNAME, USEMAIL, USEPASSWORD, ID_ROLE, USETOKEN)
+                    VALUES ('$nom', '$prenom', '$mail', '$mdp', 3, '$token')";
 
 
 
@@ -78,7 +81,29 @@ if (isset($_POST['frmRegistration'])){
 
         else{
             if(mysqli_query($connection, $requete)) {
-            echo "donnée enregistrees";
+                echo "donnée enregistrees";
+                $id = mysqli_insert_id($connection);
+
+
+                $messageMail = "<h1>wunderbar !!!</h1>";
+                $messageMail .= "<p>vous etes inscrit !</p>";
+                $messageMail .= "<p>mais vous devez valider votre inscription</p>";
+                //envoie sur serveur
+                $messageMail .= "<p><a href='http://localhost/php/index.php?page=mailValidation&amp;id=$id&amp;token=$token'>";
+                $messageMail .= "clicque moi grand fou";
+                $messageMail .= "</a></p>";
+                
+                
+
+                $headers = "From: manu@elysees.fr" . "\r\n" .
+                            "Reply-to: edouard@matignon.com" . "\r\n" .
+                            "X-Mailer: PHP/" . phpversion();
+
+                mail($mail, 'inscription compte', $messageMail, $headers);
+
+
+
+
             }
                 else {
                     echo "erreur";
