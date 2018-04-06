b6053de730090909284fab309402a592b4573945
jarvis76 <christophermonprofit@gmail.com>
2018-04-06 21:01:24 +0200

    Correction texte

---
+++ includes/frmLogin.php
@@ -0,0 +1,15 @@
+<form method="post" action="#">
+    <div>
+        <label for="mail">Login (mail) :</label>
+        <input type="text" name="mail" />
+    </div>
+    <div>
+        <label for="mdp">Mot de passe :</label>
+        <input type="password" name="mdp" />
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+    </div>
+    <input type="hidden" name="frmLogin" />
+</form>
